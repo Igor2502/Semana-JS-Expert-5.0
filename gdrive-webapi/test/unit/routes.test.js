4b6ce3b84076cff8c111ec8ba03355caa19c253b
Igor <igor.gds.00@gmail.com>
2021-09-11 10:27:32 -0300

    upload de arquivos sob demanda e 100% de code coverage com Jest

--- gdrive-webapi/test/unit/routes.test.js
+++ gdrive-webapi/test/unit/routes.test.js
@@ -1,30 +1,39 @@
-import { describe, test, expect, jest } from '@jest/globals';
+import { describe, test, expect, jest, beforeEach } from '@jest/globals';
+import { logger } from '../../src/logger.js';
+import UploadHandler from '../../src/uploadHandler.js';
+import TestUtil from '../_util/testUtil.js';
 import Routes from './../../src/routes.js'
 
 describe('#Routes suite test', () => {
 
+  const request = TestUtil.generateReadableStream(['some file bytes'])
+  const response = TestUtil.generateWritableStream(() => {})
   const defaultParams = {
-    request: {
+    request: Object.assign(request, {
       headers: {
         'Content-Type': 'multipart/form-data'
       },
       method: '',
       body: {}
-    },
-    response: {
+    }),
+    response: Object.assign(response, {
       setHeader: jest.fn(),
       writeHead: jest.fn(),
       end: jest.fn()
-    },
+    }),
     values: () => Object.values(defaultParams)
   }
+
+  beforeEach(() => {
+    jest.spyOn(logger, 'info').mockImplementation()
+  })
 
   describe('#setSocketInstance', () => {
     test('setSocket should store io instance', () => {
       const routes = new Routes()
       const ioObj = {
         to: (id) => ioObj,
-        emit: (event, message) => {}
+        emit: (event, message) => { }
       }
 
       routes.setSocketInstance(ioObj)
@@ -58,7 +67,7 @@ describe('#Routes suite test', () => {
     test('given method OPTIONS it should chose options route', async () => {
       const routes = new Routes()
       const params = {
-          ...defaultParams
+        ...defaultParams
       }
 
       params.request.method = 'OPTIONS'
@@ -115,6 +124,32 @@ describe('#Routes suite test', () => {
 
       expect(params.response.writeHead).toHaveBeenCalledWith(200)
       expect(params.response.end).toHaveBeenCalledWith(JSON.stringify(filesStatusesMock))
+    })
+  })
+
+  describe('#post', () => {
+    test('it should validate post route workflow', async () => {
+      const routes = new Routes('/tmp')
+      const options = {
+        ...defaultParams
+      }
+      options.request.method = 'POST'
+      options.request.url = '?socketId=10'
+
+      jest.spyOn(UploadHandler.prototype, UploadHandler.prototype.registerEvents.name)
+        .mockImplementation((headers, onFinish) => {
+          const writable = TestUtil.generateWritableStream(() => { })
+          writable.on('finish', onFinish)
+
+          return writable
+        })
+
+      await routes.handler(...options.values())
+
+      expect(UploadHandler.prototype.registerEvents).toHaveBeenCalled()
+      expect(options.response.writeHead).toHaveBeenCalledWith(200)
+      const expectedResult = JSON.stringify({ result: 'Files uploaded with success!'})
+      expect(options.response.end).toHaveBeenCalledWith(expectedResult)
     })
   })
 })
